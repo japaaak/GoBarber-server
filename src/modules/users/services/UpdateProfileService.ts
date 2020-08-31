de73e4c5ac243a772f112d1db0a1c82c6c384b84
JapaaAk <bbjapaa@gmail.com>
2020-08-31 12:47:49 +0900

    alt

--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -29,8 +29,8 @@ class UpdateProfileService {
     user_id,
     name,
     email,
-    password,
     old_password,
+    password,
   }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
@@ -43,6 +43,9 @@ class UpdateProfileService {
     if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
       throw new AppError('E-mail already in use.');
     }
+
+    user.name = name;
+    user.email = email;
 
     if (password && !old_password) {
       throw new AppError('Inform the old password to set a new password');
@@ -60,9 +63,6 @@ class UpdateProfileService {
 
       user.password = await this.hashProvider.generateHash(password);
     }
-
-    user.name = name;
-    user.email = email;
 
     return this.usersRepository.save(user);
   }
